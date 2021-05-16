4c49358d477651ddfa815ee6ba2b645f5c26f36b
AlexeyShimko <shimko390@mail.ru>
2021-05-16 20:16:28 +0300

    Чистовик

--- src/main/java/HWLesson2/HWLesson2/Shimko.java
+++ src/main/java/HWLesson2/HWLesson2/Shimko.java
@@ -2,25 +2,20 @@ package HWLesson2.HWLesson2;
 
 public class Shimko {
     public static void main(String[] args) {
-        System.out.println(checkMySum());
-        checkInteger();
-        System.out.println(numberPositiveOrNegative());
-        printString();
-        System.out.println(checkLeapYear());
+        System.out.println(checkMySum(11, 8));
+        checkInteger(114);
+        System.out.println(numberPositiveOrNegative(3746));
+        printString("Hello world",5);
+        System.out.println(checkLeapYear(900));
     }
 
-    public static boolean checkMySum() {
-        int a = 11;
-        int b = 6;
+    public static boolean checkMySum(int a, int b) {
 
         if (a + b >= 10 || a + b <= 20) return true;
-        else {
-            return false;
-        }
+        else return false;
     }
 
-    public static void checkInteger() {
-        int x = 114;
+    public static void checkInteger(int x) {
 
         if (x >= 0) {
             System.out.println("Число положительное");
@@ -29,25 +24,22 @@ public class Shimko {
         }
     }
 
-    public static boolean numberPositiveOrNegative() {
-        long x = -3456;
+    public static boolean numberPositiveOrNegative(int x) {
+
         if (x >= 0) return false;
         else {
             return true;
         }
-
     }
 
-    public static void printString() {
-        String w = "Hello World!";
-        int t = 8;
+    public static void printString(String w,int t) {
+
         for (int i = 0; i < t; i++) {
             System.out.println(w);
         }
     }
 
-    public static boolean checkLeapYear() {
-        long g = 700;
+    public static boolean checkLeapYear(int g) {
 
         if (g % 400 == 0) return true;
         else if (g % 4 == 0 && g % 100 !=0) {
@@ -55,6 +47,5 @@ public class Shimko {
         } else {
             return false;
         }
-
     }
 }
